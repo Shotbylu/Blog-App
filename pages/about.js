93e0b3ed1280fb1618035fb272c1aa6f4a120524
Lungelo Sibisi <lungelosibisi83@gmail.com>
2025-01-15 07:57:29 +0200

    about.js

--- pages/about.js
+++ pages/about.js
@@ -23,13 +23,13 @@ function about({ topics }) {
         <h2 className="text-3xl font-semibold text-gray-700 py-4 dark:text-gray-200">
           Hi,{" "}
           <span className="bg-indigo-400 text-white rounded px-1 dark:bg-indigo-500">
-            I’m Soumyajit
+            I’m Lungelo Sibisi
           </span>{" "}
           . Nice to meet you.
         </h2>
         <p className="text-gray-600 mt-4 dark:text-indigo-200">
-          I am a self taught developer currently pursuing IMSC in Maths and
-          Computing in BIT Mesra. My field of Interest's are building new Web
+          I am a self taught developer currently pursuing a qualification in Data Science
+          and Statistics. My field of Interest's are building new Web
           Technologies and Products and also in areas related to Deep Learning
           and Natural Launguage Processing.
         </p>
@@ -40,7 +40,7 @@ function about({ topics }) {
             <a
               rel="noopener noreferrer"
               target="_blank"
-              href="https://soumyajit.vercel.app/"
+              href="https://github.com/Shotbylu/"
             >
               See My Works
             </a>
@@ -52,7 +52,7 @@ function about({ topics }) {
               className="twitter-follow-button"
               rel="noopener noreferrer"
               target="_blank"
-              href="https://twitter.com/soumyajit4419"
+              href="https://twitter.com/shotbylu"
             >
               Follow Me
             </a>
