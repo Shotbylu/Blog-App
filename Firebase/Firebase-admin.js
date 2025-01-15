73a4a724274baac68f92f246cc8b1e06b6f69c38
Lungelo Sibisi <lungelosibisi83@gmail.com>
2025-01-15 08:59:57 +0200

    Firebase-admin.js

--- Firebase/Firebase-admin.js
+++ Firebase/Firebase-admin.js
@@ -1,13 +1,13 @@
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      project_id: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "firebase-adminsdk@blog-app-5bb5d.iam.gserviceaccount.com",
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") || "-----BEGIN PRIVATE KEY-----\nYourPrivateKeyHere\n-----END PRIVATE KEY-----",
+      projectId: process.env.FIREBASE_PROJECT_ID || "blog-app-5bb5d",
     }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    databaseURL: process.env.FIREBASE_DATABASE_URL || "https://blog-app-5bb5d.firebaseio.com",
   });
 }
 
